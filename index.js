ef61ed0518a0c9ed3bc4fb8b0548144a122d31fa
Zi <germanychinaaustralia@yahoo.com>
2020-02-13 06:20:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
 function takeANumber(currentLineArr, newPerson)  {
-  newPersonIdx = currentLineArr.length+2;
-  let greeting = `Welcome, ${newPerson}. You are number ${newPersonIdx} in line.`
-  return greeting;
-}
-
-function nowServing(currentLineArr)  {
-  if ( currentLineArr.length === 0 )  {
-    return  "There is nobody waiting to be served!";
-  }  else  {
-    return currentLineArr.shift();
+    newPersonIdx = currentLineArr.length+1;
+    let greeting = `Welcome, ${newPerson}. You are number ${newPersonIdx} in line.`
+    currentLineArr.push(newPerson);
+    return greeting;
   }
-}
 
-function currentLine(currentLineArr)  {
-  if (currentLineArr.length === 0)  {
-    return "The line is currently empty";
-  }  else  {
-    let theLine = "The line is currently: ";
-    for ( let i = 0; i<currentLineArr.length; i++ )  {
-      theLine += `${i+1}. ${currentLineArr[i]}`;
+  function nowServing(currentLineArr)  {
+    if ( currentLineArr.length === 0 )  {
+      return  "There is nobody waiting to be served!";
+    }  else  {
+      return "Currently serving " + currentLineArr.shift();
     }
-    return theLine;
   }
-}
+
+  function currentLine(currentLineArr)  {
+    if (currentLineArr.length === 0)  {
+      return "The line is currently empty";
+    }  else  {
+      let theLine = "The line is currently:";
+      for ( let i = 0; i<currentLineArr.length; i++ )  {
+        theLine += ` ${i+1}. ${currentLineArr[i]},`;  // gotta format away the last comma ugh
+      }
+      theLine = theLine.slice(0, -1);
+      return theLine;
+    }
+  }
